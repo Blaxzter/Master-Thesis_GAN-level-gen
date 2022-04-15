d351afa51b9cb748cabd086cc455fa66deb4b9b4
Frederic Marvin Abraham <f.abraham@campus.tu-berlin.de>
2022-04-15 14:09:20 +0200

    add gan example
    Former-commit-id: ff04cce88878e4ccd83de188c45350ea8bf90a49

---
+++ playground/gan_example/main.py
@@ -0,0 +1,13 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from tensorflow.keras import layers
+import time
+import tensorflow as tf
+
+from IPython import display
+
+if __name__ == '__main__':
+
+    print(f'Tenserflow Version: {tf.__version__}')
